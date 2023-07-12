34a768e0762ed9e35bab05f74c64443beed04774
Vidhi379 <70445968+Vidhi379@users.noreply.github.com>
2023-07-12 16:22:09 +0000

    variables and datatypes topic of js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+// const,let,var (var is rarely used due to issue in block and functional scope)
+const firstName = "Vidhi";
+let surname = "Shah";
+var age = 25;
+bornPlace = "Mumbai";
+let birthyear;
+age="26"
+console.table([firstName,surname,age,bornPlace,birthyear]);
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3); // this is node js, and not browser
+
+// Datatypes
+let name = "Vidhi" //string
+let age = 25 //number
+let surname; // undefined
+let temp = null; // no value recieved from weather forecast team
+
+// number => 2 to power 53 
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned to a variable 
+// symbol => unique 
+// object
+
+console.log(typeof "Vidhi") // o/p string
+console.log(typeof null) // it gives object as datatype (problem in js language)
+console.log(typeof undefined) // undefined
