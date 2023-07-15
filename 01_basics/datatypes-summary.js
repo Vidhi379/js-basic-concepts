7b87a11661f50ce20a596a10498ff775f919f49a
Vidhi379 <70445968+Vidhi379@users.noreply.github.com>
2023-07-15 08:14:29 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,23 @@
+//  there are 2 types of datatypes:
+//  1.primitive - (total 7) string,boolean,number,null,undefined,Symbol,BigInt
+// Symbol is used when we have to pass an unique id eg:
+const Id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(Id === anotherId); //false 
+
+// BigInt is used for large number and n is added as a suffix in the end of the number
+const bigNumber = 123456789000000888888888888888888n
+
+//  2.non-primitive || Reference(have place in memory for their storage) - Arrays,Objects and functions 
+const heroes = ['Ironman', 'Captain America', 'SpiderMan', 'Thor', 'Hulk', 'AntMan', 'Doctor Strange'];
+let myObj = {
+    name: 'Vidhi',
+    age: 25,
+}
+const myFunction = () =>{
+    console.log('Hello world!')
+}
+myFunction();
+
+
+
