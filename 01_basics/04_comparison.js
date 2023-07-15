eb7a04bccd232e962a5bbe7aa8dfbab36c89f66b
Vidhi379 <70445968+Vidhi379@users.noreply.github.com>
2023-07-15 07:44:55 +0000

    datatypes comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+// console.log("2">1);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined<0);
+
+// === (strict check, checks not only value also checks if the datatype is same)
+// == (here conversion happens "2" == 2 will be true!)
